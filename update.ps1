e43a2c976dfbf234d9e5e870dd104b24aaecb0d3
Chris Roadcap <capper620@gmail.com>
2023-12-09 15:01:13 -0500

    Updating automation script

--- update.ps1
+++ update.ps1
@@ -14,7 +14,25 @@ echo "
 sleep 1
 echo "Welcome to The Company automated update tool. Please stand by."
 sleep 2
+
+echo "Pulling new assets from GitHub"
+
+git reset --hard
 git pull
-mv -Force BepInEx ..
+
+
+
+sleep 2
+echo "Purging old BepInEx directory"
+cd ..
+rm -Force -r BepInEx
+
+echo "Moving updated BepInEx directory to correct location"
+cd ScrubCompanyContent
+cp -Force BepInEx ..
+sleep 2
 echo "Update complete, Thank you for being a great asset to The Company."
-sleep 3
+
+
+
+sleep 1
